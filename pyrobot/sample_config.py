4c28c9b1dacf75f6126e91615bb55850807d2da1
Sourcery AI <>
2020-11-21 16:28:43 +0000

    'Refactored by Sourcery'

--- pyrobot/sample_config.py
+++ pyrobot/sample_config.py
@@ -1,6 +1,8 @@
 import os
 
 
+
+
 class Config:
     LOGGER = True
     # The Telegram API things
@@ -37,7 +39,7 @@ class Config:
     # SuDo User
     OWNER_ID = int(os.environ.get("OWNER_ID", "7351948"))
     # Array to store users who are authorized to use the bot
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+    SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
     # the maximum number of 'selectable' messages in Telegram
     TG_MAX_SELECT_LEN = 100
     # for bakanup purposes
@@ -53,6 +55,7 @@ class Config:
         "LAYER_UPDATE_MESSAGE_CAPTION",
         None
     )
+
 
 
 class Production(Config):
