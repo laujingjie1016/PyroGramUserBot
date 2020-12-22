b4e1c0aa3dfdedd5b7abe74ca88fd02168647302
M.Furkan <byazade@yandex.com>
2020-12-22 10:54:53 +0300

    Update sample_config.py

--- pyrobot/sample_config.py
+++ pyrobot/sample_config.py
@@ -3,15 +3,14 @@ import os
 class Config(object):
     LOGGER = True
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 1714929))
-    API_HASH = os.environ.get("API_HASH", "e2036011bf3d8187a1d2a4b03fa63975")
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "abcdefghjkjhgfddfghjklkjhgf")
     # Get these values from my.telegram.org
     # string session for running on Heroku
     # some people upload their session files on GitHub or other third party hosting
     # websites, this might prevent the un-authorized use of the
-    # confidential session files
-    a = """AQCM6PQ5UmXC6cwl6UDi5bRopEoFfuPsyqQkrCTPlHMYkEwxqRAnF8CnXwQiLmlepoMd6-RPVb8qnEmLHhRsNgFcIUIN3MIPSXStIb72ZcUFifrOfgBHqsHtqUN7piAZLyAL-z1Rd4f1vCzgy-BBQ0Wu7L1Ohygu2WRvGy3FSfm13JgjkX-ol7PyKOcUNCDoc0jr_4krZXLeOx53vnv72DFrP3Bryi-ugZWilBy3LMIt-iWPxu61a80no9In--5wa9OgkB-FyGrVOFqF6xf8Fpw1ntYmUePMbJmMU5Qfkr_0sbSVKhJUWcqey-BmlestrBhMHtEsW0oYjceaS_Lq4D62SXI2zAA"""
-    HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION", a)
+    # confidential session file
+    HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION", None)
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # specify command handler that should be used for the plugins
@@ -29,10 +28,10 @@ class Config(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
     MONGO_URI = os.environ.get("MONGO_URI",None)
 
-    LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", "-1001123894584"))
+    LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", "-100112389435384"))
     # gDrive variables
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", "-1001123894584")
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", "-1001134323894584")
     PRIVATE_CHANNEL_BOT_API_ID = os.environ.get("PRIVATE_CHANNEL_BOT_API_ID", None)
     if PRIVATE_CHANNEL_BOT_API_ID:
         PRIVATE_CHANNEL_BOT_API_ID = int(PRIVATE_CHANNEL_BOT_API_ID)
